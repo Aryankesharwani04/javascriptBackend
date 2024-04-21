ebd6639a5a1ef6c06e75278e7daa00cc9a79bbf2
Aryankesharwani04 <2k22.cse.2213575@gmail.com>
2024-04-22 00:30:18 +0530

    hooks and jwt

---
+++ src/index.js
@@ -0,0 +1,59 @@
+// require('dotenv').config({path: './env'})
+
+import dotenv from "dotenv";
+dotenv.config({
+    path: './.env'
+})
+import connectDB from "./db/index.js";
+
+
+connectDB()
+.then(() => {
+    app.on("error", (err) => {
+        console.log("ERROR OCCURED", err);
+        throw err;
+    })
+    app.listen(process.env.PORT || 8000, () => {
+        console.log(`Database connected at port ${process.env.port}`);
+    })
+})
+.catch((err) => {
+    console.log("MONGODB FAILED !!!", err);
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+import express from "express";
+
+const app = express()
+
+;( async () => {
+    try{
+        await mongoose.connect(`${process.env.MONOGODB_URI}/
+        ${DB_NAME}`)
+        app.on("error", (error) => {
+            console.log("APP does not talk to database error occured: ", error);
+            throw error
+        })
+        app.listen(process.env.PORT, () => {
+            console.log(`App listening on PORT : ${process.env.PORT}`);
+        })
+    }
+    catch(error){
+        console.log("ERROR: ", error);
+        throw error
+    }
+})()
+
+*/
